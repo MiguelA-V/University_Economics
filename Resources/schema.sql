c5368bbef381eacad6445f82171345bf1b66922e
jskyler <72110869+jskyler@users.noreply.github.com>
2021-01-09 12:09:26 -0600

    tuition data imported into sql

---
+++ Resources/schema.sql
@@ -0,0 +1,16 @@
+-- Create Two Tables
+CREATE TABLE year_2_tuition (
+  year_2_id INT PRIMARY KEY,
+  start_year INT,
+  end_year INT,
+  public_tuition INT,
+  private_tuition INT
+);
+
+CREATE TABLE year_4_tuition (
+  year_4_id INT PRIMARY KEY,
+  start_year INT,
+  end_year INT,
+  public_tuition INT,
+  private_tuition INT
+);
